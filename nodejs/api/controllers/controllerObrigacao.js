a4c5bf3f671c362593cc3085a905a69da4ec8253
P2000895628 <peusoaresf@yahoo.com.br>
2018-12-05 17:06:03 +0000

    Criação da tela de Listagem de Obrigaçõe requisitadas

--- nodejs/api/controllers/controllerObrigacao.js
+++ nodejs/api/controllers/controllerObrigacao.js
@@ -155,7 +155,14 @@ module.exports = {
 			' tblObrigacaoAcessoria."id_obrigacao_acessoria" tblObrigacaoAcessoriaID , ' +
 			' tblObrigacaoAcessoria."nome" tblObrigacaoAcessoriaNome , ' + ' tblObrigacaoAcessoria."data_inicio" tblObrigacaoAcessoriaDTInicio , ' +
 			' tblObrigacaoAcessoria."fk_dominio_obrigacao_acessoria_tipo.id_dominio_obrigacao_acessoria_tipo" tblObrigacaoAcessoriaIdTipo ,  ' +
-			' tblObrigacaoAcessoria."data_fim" tblObrigacaoAcessoriaDTFim , ' + ' tblAnoFiscal.* ' + ' from "VGT.OBRIGACAO" tblObrigacao ' +
+			' tblObrigacaoAcessoria."data_fim" tblObrigacaoAcessoriaDTFim , ' + ' tblAnoFiscal.*, ' +
+			' ( Select Count(*) From "VGT.OBRIGACAO") "Total", ' +
+			' ( Select Count(*) From "VGT.OBRIGACAO" Where "fk_dominio_status_obrigacao.id_status_obrigacao"=1) "NIniciada", ' +
+			' ( Select Count(*) From "VGT.OBRIGACAO" Where "fk_dominio_status_obrigacao.id_status_obrigacao"=2) "Aguardado", ' +
+			' ( Select Count(*) From "VGT.OBRIGACAO" Where "fk_dominio_status_obrigacao.id_status_obrigacao"=3) "Atraso", ' +
+			' ( Select Count(*) From "VGT.OBRIGACAO" Where "fk_dominio_status_obrigacao.id_status_obrigacao"=4) "Entregue", ' +
+			' ( Select Count(*) From "VGT.OBRIGACAO" Where "fk_dominio_status_obrigacao.id_status_obrigacao"=5) "EntregueFP" ' +
+			' from "VGT.OBRIGACAO" tblObrigacao ' +
 			' left outer join "VGT.DOMINIO_STATUS_OBRIGACAO" tblStatusObrigacao ' +
 			' on tblObrigacao."fk_dominio_status_obrigacao.id_status_obrigacao" = tblStatusObrigacao."id_status_obrigacao" ' +
 			' left outer join "VGT.DOMINIO_PAIS" tblPais ' + ' on tblObrigacao."fk_dominio_pais.id_dominio_pais" = tblPais."id_dominio_pais" ' +
@@ -165,53 +172,53 @@ module.exports = {
 			' left Outer Join "VGT.OBRIGACAO_ACESSORIA" tblObrigacaoAcessoria ' +
 			' On tblObrigacao."fk_obrigacao_acessoria.id_obrigacao_acessoria" = tblObrigacaoAcessoria."id_obrigacao_acessoria" ' +
 			' left Outer Join "VGT.DOMINIO_ANO_FISCAL" tblAnoFiscal ' +
-			' On tblObrigacao."fk_dominio_ano_fiscal.id_dominio_ano_fiscal" = tblAnoFiscal."id_dominio_ano_fiscal" ' ;
-			
-			var oWhere = [];
-			var aParams = [];
-	
-			if (req.query.idEmpresa) {
-				oWhere.push(' tblEmpresa."id_empresa" = ? ');
-				aParams.push(req.query.idEmpresa);
-			}
-			
-			if (req.query.idTipo) {
-				oWhere.push(' tblObrigacaoAcessoria."id_obrigacao_acessoria" = ? ');
-				aParams.push(req.query.idTipo);
-			}
-			
-			if (req.query.idStatus) {
-				oWhere.push(' tblObrigacao."fk_dominio_status_obrigacao.id_status_obrigacao" = ? ');
-				aParams.push(req.query.idStatus);
-			}
-			
-			if (req.query.idAnoFiscal) {
-				oWhere.push(' tblObrigacao."fk_dominio_ano_fiscal.id_dominio_ano_fiscal" = ? ');
-				aParams.push(req.query.idAnoFiscal);
-			}
-	
-			if (oWhere.length > 0) {
-				sStatement += "where ";
-	
-				for (var i = 0; i < oWhere.length; i++) {
-					if (i !== 0) {
-						sStatement += " and ";
-					}
-					sStatement += oWhere[i];
+			' On tblObrigacao."fk_dominio_ano_fiscal.id_dominio_ano_fiscal" = tblAnoFiscal."id_dominio_ano_fiscal" ';
+
+		var oWhere = [];
+		var aParams = [];
+
+		if (req.query.idEmpresa) {
+			oWhere.push(' tblEmpresa."id_empresa" = ? ');
+			aParams.push(req.query.idEmpresa);
+		}
+
+		if (req.query.idTipo) {
+			oWhere.push(' tblObrigacaoAcessoria."id_obrigacao_acessoria" = ? ');
+			aParams.push(req.query.idTipo);
+		}
+
+		if (req.query.idStatus) {
+			oWhere.push(' tblObrigacao."fk_dominio_status_obrigacao.id_status_obrigacao" = ? ');
+			aParams.push(req.query.idStatus);
+		}
+
+		if (req.query.idAnoFiscal) {
+			oWhere.push(' tblObrigacao."fk_dominio_ano_fiscal.id_dominio_ano_fiscal" = ? ');
+			aParams.push(req.query.idAnoFiscal);
+		}
+
+		if (oWhere.length > 0) {
+			sStatement += "where ";
+
+			for (var i = 0; i < oWhere.length; i++) {
+				if (i !== 0) {
+					sStatement += " and ";
 				}
+				sStatement += oWhere[i];
 			}
-		
-			sStatement += ' Order By tblEmpresa."id_empresa"';
-			
-			model.execute({
-				statement: sStatement,
-				parameters: aParams
-			}, function (err, result) {
-				if (err) {
-					res.send(JSON.stringify(err));
-				} else {
-					res.send(JSON.stringify(result));
-				}
-			});
+		}
+
+		sStatement += ' Order By tblEmpresa."id_empresa"';
+
+		model.execute({
+			statement: sStatement,
+			parameters: aParams
+		}, function (err, result) {
+			if (err) {
+				res.send(JSON.stringify(err));
+			} else {
+				res.send(JSON.stringify(result));
+			}
+		});
 	}
 };
---
+++ ui5/webapp/controller/compliance/ListagemRequisicoes.controller.js
@@ -0,0 +1,151 @@
+sap.ui.define(
+	[
+		"ui5ns/ui5/controller/BaseController",
+		"ui5ns/ui5/model/formatter",
+		"ui5ns/ui5/lib/NodeAPI"
+	],
+	function (BaseController, formatter, NodeAPI) {
+		BaseController.extend("ui5ns.ui5.controller.compliance.ListagemRequisicoes", {
+			formatter: formatter,
+			
+			onInit: function () {
+				this.setModel(new sap.ui.model.json.JSONModel({
+					requisicoes: [
+						/*{
+							id: 1,
+							empresa: "Empresa A",
+							trimestre: "1º Trimestre",
+							dataRequisicao: "20/05/2018",
+							anoRequisicao: 2018,
+							usuario: "Usuario A",
+							justificativa: "Não consegui fazer a tempo",
+							status: {
+								icone: "sap-icon://accept",
+								cor: "green", 
+								tooltip: "Aprovado"
+							}
+						},
+						{
+							id: 2,
+							empresa: "Empresa A",
+							trimestre: "2º Trimestre",
+							dataRequisicao: "10/05/2018",
+							anoRequisicao: 2018,
+							usuario: "Usuario A",
+							justificativa: "Esqueci de preencher com o valor final",
+							resposta: "Deveria ter preenchido.",
+							status: {
+								icone: "sap-icon://decline",
+								cor: "red",
+								tooltip: "Rejeitado"
+							}
+						},
+						{
+							id: 3,
+							empresa: "Empresa A",
+							trimestre: "3º Trimestre",
+							dataRequisicao: "25/05/2018",
+							anoRequisicao: 2018,
+							usuario: "Usuario A",
+							justificativa: "Não consegui fazer a tempo",
+							status: {
+								icone: "sap-icon://lateness",
+								cor: "orange",
+								tooltip: "Em andamento"
+							}
+						}*/	
+					]
+				}));
+				this.getRouter().getRoute("ttcRequisicaoReabertura").attachPatternMatched(this._onRouteMatched, this);
+			},
+			
+			onTrocarStatus: function (oEvent) {
+				// sap.m.MessageToast.show("Ano selecionado: " + oEvent.getSource().getSelectedItem().getText());
+				
+				this._atualizarDados();
+			},
+			
+			onNovoObjeto: function (oEvent) {
+				this.getRouter().navTo("ttcFormularioNovaRequisicaoReabertura");
+			},
+			
+			navToHome: function () {
+				this.getRouter().navTo("selecaoModulo");
+			},
+			
+			navToPage2: function () {
+				this.getRouter().navTo("ttcListagemEmpresas");
+			},
+			
+			navToPage3: function () {
+				//this.getRouter().navTo("ttcResumoTrimestre");
+				this._navToResumoTrimestre();
+			},
+			
+			_navToResumoTrimestre: function () {
+				//this._limparModel();
+				
+				var oEmpresaSelecionada = this.getModel().getProperty("/empresa");
+				var sIdAnoCalendario = this.getModel().getProperty("/idAnoCalendario");
+			
+				this.getRouter().navTo("ttcResumoTrimestre", {
+					oEmpresa: JSON.stringify(oEmpresaSelecionada),
+					idAnoCalendario: sIdAnoCalendario
+				}); 
+			},
+			
+			_onRouteMatched: function (oEvent) {
+				var that = this;
+				
+				var oParametros = JSON.parse(oEvent.getParameter("arguments").parametros);
+				
+				this.getModel().setProperty("/empresa", oParametros.empresa);
+				this.getModel().setProperty("/idAnoCalendario", oParametros.anoCalendario);
+				
+				NodeAPI.listarRegistros("RequisicaoReaberturaStatus", function (response) {
+					if (response) {
+						response.unshift({});
+						that.getModel().setProperty("/RequisicaoReaberturaStatus", response);
+						
+						that._atualizarDados();
+					}
+				});
+			},
+			
+			_atualizarDados: function (oEvent) {
+				var that = this;
+				
+				var oEmpresa = this.getModel().getProperty("/empresa");
+				var sIdStatus = this.getModel().getProperty("/RequisicaoReaberturaStatusSelecionado") ? this.getModel().getProperty("/RequisicaoReaberturaStatusSelecionado") : "";
+				
+				NodeAPI.listarRegistros("DeepQuery/RequisicaoReabertura?status=" + sIdStatus +"&empresa=" + oEmpresa.id_empresa , function(response){
+					if (response) {
+						for (var i = 0; i < response.length; i++) {
+							if (response[i].id_dominio_requisicao_reabertura_status === 1) {
+							    response[i].oStatus = {
+								  icone: "sap-icon://lateness",
+							      cor: "orange",
+							      tooltip: "Aguardando"
+							    };
+							} else if (response[i].id_dominio_requisicao_reabertura_status === 2) {
+							    response[i].oStatus = {
+								  icone: "sap-icon://accept",
+							      cor: "green",
+							      tooltip: "Aprovado"
+							    };
+							} else if (response[i].id_dominio_requisicao_reabertura_status === 3) {
+							    response[i].oStatus = {
+								  icone: "sap-icon://decline",
+							      cor: "red",
+							      tooltip: "Reprovado"
+							    };
+							}
+						}
+						that.getModel().setProperty("/requisicoes", response);
+					}
+				});
+			}
+			
+		});
+	}
+);
