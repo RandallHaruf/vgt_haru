1cc3dc00f997433beb45dbcfc1cd478124a787e2
P2000968011 <joao.fernandes@tenti.com.br>
2018-12-12 16:35:21 +0000

    Funcao stringDataDoBancoParaStringDDMMYYYY para Utils

--- ui5/webapp/lib/Utils.js
+++ ui5/webapp/lib/Utils.js
@@ -26,6 +26,12 @@ sap.ui.define(
 				var fDataNoPadrao =  Data.getDate().toString().padStart(2,"0") + "/" +(Data.getMonth() +1).toString().padStart(2,"0") + "/" + Data.getFullYear().toString();
 				return fDataNoPadrao;
 			},
+
+			stringDataDoBancoParaStringDDMMYYYY: function (dataString){
+				//PASSAR DIRETO DO BANCO NO FORMATO "yyyy-MM-dd"
+				var DataFinal = dataString.substring(8,10)+"/"+dataString.substring(5,7)+"/"+dataString.substring(4,0);
+				return DataFinal;
+			},
 			bancoParaJsDate: function (dataString){
 				//PASSAR DIRETO DO BANCO NO FORMATO "yyyy-MM-dd"
 				var DataFinal = new Date(dataString.substring(0,4),dataString.substring(5,7)-1,dataString.substring(8,10));
