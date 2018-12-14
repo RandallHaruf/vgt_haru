6afa747483a022915ee2175936be02af960afdfa
P2000965014 <luiz.junior@tenti.com.br>
2018-12-14 15:09:46 +0000

    A Deep Query da tabela de Ligação entre o Modelo e a Empresa

--- nodejs/api/controllers/controllerRelModeloEmpresa.js
+++ nodejs/api/controllers/controllerRelModeloEmpresa.js
@@ -100,17 +100,70 @@ module.exports = {
 	deepQuery: function (req, res) {
 		res.send("TODO: DeepQuery da Entidade RelModeloEmpresa");
 
-		/*var sStatement = 'select * from "DUMMY"';
+		var sStatement =
+			' SELECT ' 
+			+ ' ME."fk_id_modelo_obrigacao.id_modelo", '
+			+ ' ME."fk_id_empresa.id_empresa", '
+	        + ' EM."nome", '
+			+ ' OB."nome_obrigacao", '
+			+ ' OB."fk_id_dominio_obrigacao_status.id_dominio_obrigacao_status"'
+			+ ' ME."id_rel_modelo_empresa", '
+			+ ' ME."fk_id_dominio_obrigacao_status.id_dominio_obrigacao_status", '
+			+ ' SO."descricao",'
+	    	+ ' ME."prazo_entrega_customizado" '
+			+ ' FROM "VGT.REL_MODELO_EMPRESA" ME ' 
+			+ ' inner join "VGT.EMPRESA" EM on ME."fk_id_empresa.id_empresa" = EM."id_empresa" '
+			+ ' inner join "VGT.MODELO_OBRIGACAO" OB on ME."fk_id_modelo_obrigacao.id_modelo" = OB."id_modelo"'
+			+ ' inner join "VGT.DOMINIO_STATUS_OBRIGACAO" SO on ME."fk_id_dominio_obrigacao_status.id_dominio_obrigacao_status" = SO."id_status_obrigacao"';
+			
+			var oWhere = [];
+			var aParams = [];
+			
+		if (req.query.status) {
+			oWhere.push(' ME."fk_id_dominio_obrigacao_status.id_dominio_obrigacao_status" = ? ');
+			aParams.push(req.query.status);
+		}
+		
+		if (req.query.idempresa) {
+			oWhere.push(' ME."fk_id_empresa.id_empresa" = ? ');
+			aParams.push(req.query.idempresa);
+		}
+		
+		if (req.query.idmodelo) {
+			oWhere.push(' ME."fk_id_modelo_obrigacao.id_modelo" = ? ');
+			aParams.push(req.query.idmodelo);
+		}
+		
+		
+		if (req.query.statusModulo) {
+			oWhere.push(' OB."fk_id_dominio_obrigacao_status.id_dominio_obrigacao_status" = ? ');
+			aParams.push(req.query.statusModulo);
+		}
+		
+		
+			if (oWhere.length > 0) {
+			sStatement += "where ";
+			
+			for (var i = 0; i < oWhere.length; i++) {
+				if (i !== 0) {
+					sStatement += " and ";
+				}
+				sStatement += oWhere[i];
+			}
+		}			
 
 		model.execute({
-		statement: sStatement
+			statement: sStatement,
+			parameters: aParams
 		}, function (err, result) {
-		if (err) {
-		res.send(JSON.stringify(err));
-		}
-		else {
-		res.send(JSON.stringify(result));
-		}
-		});*/
+			if (err) {
+				res.send(JSON.stringify(err));
+			} else {
+				for (var i = 0, length = result.length; i < length; i++) {
+					result[i].ind_nao_aplicavel = result[i].ind_nao_aplicavel ? true : false;
+				}
+				res.send(JSON.stringify(result));
+			}
+		});
 	}
 };
