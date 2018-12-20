ba1cc6277094bf678a990fadd94de2976dc9a797
P2000968011 <joao.fernandes@tenti.com.br>
2018-12-20 10:46:22 +0000

    Fix no Field de pesquisa de Obrigacoes Acessorias na Tela Empresa
    Adição de Placeholders traduzidos para os campos de busca nas telas do Admin:
    - Empresa
    - CadastroNameOfTaxTTC

--- ui5/webapp/controller/admin/Empresa.controller.js
+++ ui5/webapp/controller/admin/Empresa.controller.js
@@ -17,7 +17,7 @@ sap.ui.define(
 	
 				if (sQuery !== null) {
 					this._oTxtFilter = new sap.ui.model.Filter([
-						new sap.ui.model.Filter("nome", sap.ui.model.FilterOperator.Contains, sQuery)
+						new sap.ui.model.Filter("tblModeloObrigacao.nome_obrigacao", sap.ui.model.FilterOperator.Contains, sQuery)
 					], false);
 				}
 	
