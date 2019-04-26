e51b7345025d366ef145e062b206a9fb3396000b
Pedro Ferraz <pedro.ferraz@tenti.com.br>
2019-04-26 17:59:16 +0000

    ajuste de rota !!!

--- ui5/webapp/model/Constants.js
+++ ui5/webapp/model/Constants.js
@@ -6,7 +6,8 @@ sap.ui.define(
 			//urlBackend: "https://p2000895628trial-trial-dev-nodejs.cfapps.eu10.hana.ondemand.com/node/" // app publicado na nuvem peusoaresf@yahoo.com.br endpoint eu10
 			//urlBackend: "https://dvmpa6yjbo5a0zzv-vgt-nodejs.cfapps.eu10.hana.ondemand.com/node/" // app de dev da nuvem peusoaresf@yahoo.com.br endpoint eu10
 			//urlBackend: "https://p2000950797trial-sdomainvgt-spcvgt-nodejs.cfapps.eu10.hana.ondemand.com/node/" //app de prod haruf.randall@gmail.com endpoint eu10
-			urlBackend: "https://tmsdusjgnfpx1oyf-vgt-haru-nodejs.cfapps.eu10.hana.ondemand.com/node/" // LOCAL
+			//urlBackend: "https://tmsdusjgnfpx1oyf-vgt-haru-nodejs.cfapps.eu10.hana.ondemand.com/node/" // LOCAL
+			//urlBackend: "https://digitoanyvfok8mf-vgt-haru-nodejs.cfapps.eu10.hana.ondemand.com/node/" 
 		};
 	}
 );
